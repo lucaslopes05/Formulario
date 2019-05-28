318716858eafcc11fe45f3af98d934af184aa2e1
Lucas Andre <lucaslopes05@gmail.com>
2019-05-28 15:57:42 -0300

    Correcao de codigo

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,15 @@
-function TratadorDeFormulario(){
-    var idade;
-    self = this;
-    
-    this.exibeValorIdade = function(){
-        this.idade = document.getElementById('idade').value;
-        console.log(this.idade);        
-    }
+var tratadorDeFormulario = new TratadorDeFormulario();
 
-    this.salvar = function(){
-        this.exibeValorIdade();
+function TratadorDeFormulario(event){
+  
+    // this.mutiplicaIdadePeloValorPassado = function( valor){
+    //     var minhaIdade = document.getElementById('idade').value;
+    //     var idadeTotal = valor * minhaIdade;
+    //     console.log('Retorno da Funcao MultiplicaIdade ', idadeTotal);
+    //     console.log('Teste 1 ' + self);
+    //     console.log('teste 2', self);
+    // }
 
-    }
-
-    this.mutiplicaIdadePeloValorPassado = function( valor){
-        var minhaIdade = document.getElementById('idade').value;
-        var idadeTotal = valor * minhaIdade;
-        console.log('Retorno da Funcao MultiplicaIdade ', idadeTotal);
-        console.log('Teste 1 ' + self);
-        console.log('teste 2', self);
-    }
-
-    document.getElementById('salvar').addEventListener("click",function(){
-        console.log('teste salvar');
-        self.salvar();
-        self.mutiplicaIdadePeloValorPassado(10);
-    })
 
     this.aceitaSomenteNumeros = function(event){
         if(!isNaN(String.fromCharCode(event.keyCode))){
@@ -35,4 +20,70 @@ function TratadorDeFormulario(){
     }
 }
 
-var tratadorDeFormulario = new TratadorDeFormulario();
+var form  = document.querySelector('#formulario');
+
+function obtemDadosDoFormulario(form){
+var dadosDoFormulario={
+    nome: form.nome.value,
+    sobreNome: form.sobrenome.value,
+    idade: form.idade.value,
+    cpf: form.cpf.value,
+    endereco: form.endereco.value,
+    estado: form.estado.value,
+    cep: form.cep.value,
+} 
+return dadosDoFormulario;
+}
+
+document.getElementById('salvar').addEventListener("click",function(event){
+    event.preventDefault();
+    // console.log('teste salvar');
+    var dadosDoForm = obtemDadosDoFormulario(form);
+    console.log(dadosDoForm);
+
+    var divAlerta = document.querySelector('#div-Alerta');
+    console.log('buscando do html', divAlerta);
+    
+    var alertaPrenchido = criaMensagemDoFormulario(dadosDoForm);
+    console.log(alertaPrenchido);
+    divAlerta.appendChild(alertaPrenchido);
+    
+    
+    form.reset();
+});
+
+function criaMensagemDoFormulario(dadosDoForm){
+
+    var divAlert = document.createElement('div');
+    if(verificarMaioridade(dadosDoForm))
+        divAlert.classList.add("alert-success");
+    else
+        divAlert.classList.add("alert-danger");
+    
+    
+    var tituloH4 = document.createElement('h4');
+    tituloH4.classList.add("alert-heading");
+    var paragrafo = document.createElement('p');
+    paragrafo.classList.add("mb-0");
+
+    tituloH4.textContent = dadosDoForm.sobreNome+" "+dadosDoForm.nome;
+    // tituloH4.textContent = dadosDoForm.sobreNome;
+
+    paragrafo.textContent = dadosDoForm.cpf;
+
+    divAlert.appendChild(tituloH4);
+    divAlert.appendChild(paragrafo);
+    
+    return divAlert;
+}
+
+function verificarMaioridade(dadosDoForm){
+
+    if(dadosDoForm.idade > 18){
+        return true;
+    }else{
+        if(dadosDoForm.idade <= 18)
+            return false;
+    }
+}
+
